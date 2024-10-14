318a675cd016aab7e376537a50a960510da3e764
lazarodamasceno <Mylittleponey1@>
2024-10-14 17:52:40 -0300

    Changed CustomerRegistrationRequestDto's field requestDto as personRegistrationDto

--- v1/src/main/java/com/api/v1/customer/dtos/CustomerRegistrationRequestDto.java
+++ v1/src/main/java/com/api/v1/customer/dtos/CustomerRegistrationRequestDto.java
@@ -3,6 +3,6 @@ package com.api.v1.customer.dtos;
 import com.api.v1.person.dtos.PersonRegistrationRequestDto;
 
 public record CustomerRegistrationRequestDto(
-        PersonRegistrationRequestDto requestDto
+        PersonRegistrationRequestDto personRegistrationDto
 ) {
 }
