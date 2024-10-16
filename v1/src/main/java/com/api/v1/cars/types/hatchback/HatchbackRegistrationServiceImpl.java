ee2c177093eaf22c2f347eba80434ca1bfdad8a0
lazarodamasceno <Mylittleponey1@>
2024-10-16 18:58:36 -0300

    Created the service HatchbackRegistrationServiceImpl

---
+++ v1/src/main/java/com/api/v1/cars/types/hatchback/HatchbackRegistrationServiceImpl.java
@@ -0,0 +1,33 @@
+package com.api.v1.cars.types.hatchback;
+
+import com.api.v1.cars.domain.CarRepository;
+import com.api.v1.cars.dtos.CarResponseDto;
+import com.api.v1.cars.interfaces.CarRegistrationService;
+import com.api.v1.cars.types.sedan.SedanResponseMapperUtil;
+import com.api.v1.vehicles.dtos.VehicleRegistrationRequestDto;
+import com.api.v1.vehicles.services.VehicleRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class HatchbackRegistrationServiceImpl implements CarRegistrationService {
+
+    @Autowired
+    private VehicleRegistrationService vehicleRegistrationService;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private HatchbackResponseMapperUtil hatchbackResponseMapperUtil;
+
+    @Override
+    public Mono<CarResponseDto> register(VehicleRegistrationRequestDto requestDto) {
+        return vehicleRegistrationService
+                .register(requestDto)
+                .flatMap(vehicle -> carRepository.save(Hatchback.of(vehicle)))
+                .flatMap(hatchback -> Mono.just(hatchbackResponseMapperUtil.map(hatchback)));
+    }
+
+}
