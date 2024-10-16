81ebaef477c25e4168f17fc5ace33416bca60e78
lazarodamasceno <Mylittleponey1@>
2024-10-16 02:08:05 -0300

    Created the util SuvResponseMapperUtil

---
+++ v1/src/main/java/com/api/v1/cars/types_of_car/suvs/SuvResponseMapperUtil.java
@@ -0,0 +1,23 @@
+package com.api.v1.cars.types_of_car.suvs;
+
+import com.api.v1.cars.dtos.CarResponseDto;
+import com.api.v1.cars.generics.CarResponseMapper;
+import com.api.v1.vehicles.utils.VehicleResponseMapperUtil;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+
+@Component
+public class SuvResponseMapperUtil implements CarResponseMapper<SUV> {
+
+    @Override
+    public CarResponseDto map(@NotNull SUV vehicle) {
+        return new CarResponseDto(
+                vehicle.getType(),
+                VehicleResponseMapperUtil.map(vehicle.getVehicle()),
+                ZonedDateTime.ofInstant(vehicle.getCreatedAt(), vehicle.getCreatedAtZone())
+        );
+    }
+
+}
