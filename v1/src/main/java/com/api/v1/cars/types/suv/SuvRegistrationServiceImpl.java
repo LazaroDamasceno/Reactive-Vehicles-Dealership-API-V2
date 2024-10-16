5d9233b48dbdea6d9ad02965151d73963e21cf11
lazarodamasceno <Mylittleponey1@>
2024-10-16 16:00:29 -0300

    Changed SuvRegistrationServiceImpl

--- v1/src/main/java/com/api/v1/cars/types/suv/SuvRegistrationServiceImpl.java
+++ v1/src/main/java/com/api/v1/cars/types/suv/SuvRegistrationServiceImpl.java
@@ -3,11 +3,10 @@ package com.api.v1.cars.types.suv;
 import com.api.v1.cars.domain.CarRepository;
 import com.api.v1.cars.dtos.CarResponseDto;
 import com.api.v1.cars.interfaces.CarRegistrationService;
-import com.api.v1.vehicles.domain.inventory.VehicleInventory;
-import com.api.v1.vehicles.domain.inventory.VehicleInventoryRepository;
 import com.api.v1.vehicles.domain.vehicle.Vehicle;
 import com.api.v1.vehicles.domain.vehicle.VehicleRepository;
 import com.api.v1.vehicles.dtos.VehicleRegistrationRequestDto;
+import com.api.v1.vehicles.services.VehicleInventoryingService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,7 +19,7 @@ public class SuvRegistrationServiceImpl implements CarRegistrationService {
     private VehicleRepository vehicleRepository;
 
     @Autowired
-    private VehicleInventoryRepository vehicleInventoryRepository;
+    private VehicleInventoryingService vehicleInventoryingService;
 
     @Autowired
     private CarRepository carRepository;
@@ -32,15 +31,7 @@ public class SuvRegistrationServiceImpl implements CarRegistrationService {
     public Mono<CarResponseDto> register(@Valid VehicleRegistrationRequestDto requestDto) {
         return vehicleRepository
                 .save(Vehicle.of(requestDto))
-                .flatMap(vehicle -> vehicleInventoryRepository
-                        .findByVehicle(vehicle)
-                        .switchIfEmpty(Mono.defer(() -> vehicleInventoryRepository
-                                .save(VehicleInventory.of(vehicle))
-                        ))
-                        .flatMap(inventory -> {
-                            inventory.increaseAvailableQuantity();
-                            return vehicleInventoryRepository.save(inventory);
-                        })
+                .flatMap(vehicle -> vehicleInventoryingService.addToTheInventory(vehicle)
                         .then(Mono.defer(() -> carRepository.save(SUV.of(vehicle))))
                         .flatMap(suv -> Mono.just(responseMapper.map(suv)))
                 );
