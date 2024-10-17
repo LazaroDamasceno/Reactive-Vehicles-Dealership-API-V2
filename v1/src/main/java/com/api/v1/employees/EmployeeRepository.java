a1649cc7d7dc7a328788d08475a0ad1e0e6de3dd
lazarodamasceno <Mylittleponey1@>
2024-10-17 18:49:55 -0300

    Created the annotation VIN

--- v1/src/main/java/com/api/v1/employees/EmployeeRepository.java
+++ v1/src/main/java/com/api/v1/employees/EmployeeRepository.java
@@ -1,8 +1,15 @@
 package com.api.v1.employees;
 
+import com.api.v1.persons.domain.Person;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface EmployeeRepository extends ReactiveMongoRepository<Employee, UUID> {
+
+    @Query("{ 'person': ?0 }")
+    Mono<Employee> findByPerson(Person person);
+
 }
