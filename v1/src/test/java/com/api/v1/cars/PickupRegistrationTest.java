99c460fbff43ffd4f64949b65022d68bfa75c88a
lazarodamasceno <Mylittleponey1@>
2024-10-18 16:11:49 -0300

    Created the test class ElectricRegistrationTest, HatchbackRegistrationTest, PickupRegistrationTest, SedanRegistrationTest, SuvRegistrationTest

---
+++ v1/src/test/java/com/api/v1/cars/PickupRegistrationTest.java
@@ -0,0 +1,30 @@
+package com.api.v1.cars;
+
+import com.api.v1.vehicles.dtos.VehicleRegistrationRequestDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PickupRegistrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void testSuccessful() {
+        var requestDto = new VehicleRegistrationRequestDto(
+                "Acadia",
+                "GMC",
+                2024
+        );
+        webTestClient
+                .post()
+                .uri("api/v1/cars/pickup")
+                .bodyValue(requestDto)
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+}
