c6be1d5c75cb98b255a5d4e9b0c8f0537e43df36
lazarodamasceno <Mylittleponey1@>
2024-10-18 00:23:16 -0300

    Created the repository CarRepository

---
+++ v1/src/main/java/com/api/v1/cars/CarRepository.java
@@ -0,0 +1,8 @@
+package com.api.v1.cars;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import java.util.UUID;
+
+public interface CarRepository extends ReactiveMongoRepository<Car, UUID> {
+}
