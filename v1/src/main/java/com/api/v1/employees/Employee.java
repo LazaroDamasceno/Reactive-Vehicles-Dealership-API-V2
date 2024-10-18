78a0a4714152becabb4b0aeae265338abba4ed60
lazarodamasceno <Mylittleponey1@>
2024-10-17 21:43:37 -0300

    Deleted some files

--- v1/src/main/java/com/api/v1/employees/Employee.java
+++
@@ -1,4 +0,0 @@
-package com.api.v1.employees;
-
-public class Employee {
-}
--- v1/src/main/java/com/api/v1/employees/exceptions/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.api.v1.employees.exceptions;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException() {
-        super("Employee was not found.");
-    }
-}
