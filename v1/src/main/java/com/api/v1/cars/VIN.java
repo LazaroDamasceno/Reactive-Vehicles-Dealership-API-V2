911a6a6cf95a8530b2cc8a2129449f6bc7898fea
lazarodamasceno <Mylittleponey1@>
2024-10-18 00:21:35 -0300

    Added the annotation VIN

---
+++ v1/src/main/java/com/api/v1/cars/VIN.java
@@ -0,0 +1,17 @@
+package com.api.v1.cars;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@NotNull
+@Size(min = 17, max = 17)
+public @interface VIN {
+
+}
