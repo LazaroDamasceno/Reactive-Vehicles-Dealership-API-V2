7884cf92c1bd904fe0763b3d2a18a7d282c6043d
lazarodamasceno <Mylittleponey1@>
2024-10-18 01:58:56 -0300

    Created the service HatchbackRegistrationServiceImpl

--- v1/src/main/java/com/api/v1/cars/controllers/CarController.java
+++ v1/src/main/java/com/api/v1/cars/controllers/CarController.java
@@ -4,10 +4,10 @@ import com.api.v1.cars.dtos.CarResponseDto;
 import com.api.v1.cars.services.CarRetrievalService;
 import com.api.v1.cars.services.crossovers.CrossoverRegistrationServiceImpl;
 import com.api.v1.cars.services.elecrics.ElectricRegistrationServiceImpl;
+import com.api.v1.cars.services.hatchback.HatchbackRegistrationServiceImpl;
 import com.api.v1.vehicles.dtos.VehicleRegistrationRequestDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -21,6 +21,7 @@ public class CarController {
     private final CrossoverRegistrationServiceImpl crossoverRegistrationService;
     private final CarRetrievalService carRetrievalService;
     private final ElectricRegistrationServiceImpl electricRegistrationServiceImpl;
+    private final HatchbackRegistrationServiceImpl hatchbackRegistrationService;
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
@@ -36,8 +37,14 @@ public class CarController {
 
     @PostMapping("electric")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public Mono<CarResponseDto> register(@Valid @RequestBody VehicleRegistrationRequestDto requestDto) {
+    public Mono<CarResponseDto> registerElectric(@Valid @RequestBody VehicleRegistrationRequestDto requestDto) {
         return electricRegistrationServiceImpl.register(requestDto);
+    }
+
+    @PostMapping("hatchback")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<CarResponseDto> registerHatchback(@Valid @RequestBody VehicleRegistrationRequestDto requestDto) {
+        return hatchbackRegistrationService.register(requestDto);
     }
 
 }
---
+++ v1/src/main/java/com/api/v1/cars/services/hatchback/HatchbackRegistrationServiceImpl.java
@@ -0,0 +1,31 @@
+package com.api.v1.cars.services.hatchback;
+
+import com.api.v1.cars.domain.repository.CarRepository;
+import com.api.v1.cars.domain.subclasses.Hatchback;
+import com.api.v1.cars.dtos.CarResponseDto;
+import com.api.v1.cars.services.CarRegistrationService;
+import com.api.v1.cars.utils.CarResponseMapperUtil;
+import com.api.v1.vehicles.dtos.VehicleRegistrationRequestDto;
+import com.api.v1.vehicles.services.VehicleRegistrationService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class HatchbackRegistrationServiceImpl extends CarRegistrationService {
+
+    public HatchbackRegistrationServiceImpl(
+            VehicleRegistrationService vehicleRegistrationService,
+            CarRepository carRepository
+    ) {
+        super(vehicleRegistrationService, carRepository);
+    }
+
+    @Override
+    public Mono<CarResponseDto> register(@Valid VehicleRegistrationRequestDto requestDto) {
+        return vehicleRegistrationService
+                .register(requestDto)
+                .flatMap(vehicle -> carRepository.save(Hatchback.of(vehicle)))
+                .flatMap(CarResponseMapperUtil::mapToMono);
+    }
+}
