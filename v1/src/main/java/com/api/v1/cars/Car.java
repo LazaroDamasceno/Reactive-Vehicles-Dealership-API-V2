3ee30df3784545861e6033f32ac95049aff37ded
lazarodamasceno <Mylittleponey1@>
2024-10-18 00:22:26 -0300

    Created the class Car

--- v1/src/main/java/com/api/v1/cars/Car.java
+++ v1/src/main/java/com/api/v1/cars/Car.java
@@ -1,4 +1,25 @@
 package com.api.v1.cars;
 
+import com.api.v1.vehicles.domain.Vehicle;
+import com.api.v1.vehicles.utils.VehiclePlateNumberGeneratorUtil;
+import com.api.v1.vehicles.utils.VehicleVinGeneratorUtil;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class Car {
+
+    private String type;
+    private String vin;
+    private String plateNumber;
+    private Vehicle vehicle;
+
+    public Car(String type, Vehicle vehicle) {
+        this.type = type;
+        this.vin = VehicleVinGeneratorUtil.generate();
+        this.plateNumber = VehiclePlateNumberGeneratorUtil.generate();
+        this.vehicle = vehicle;
+    }
+
 }
