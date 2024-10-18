2f4465c4243f06f53957ecc08d7d7098919a2a44
lazarodamasceno <Mylittleponey1@>
2024-10-18 17:33:15 -0300

    Created the repository EmployeeRepository

---
+++ v1/src/main/java/com/api/v1/employees/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.api.v1.employees;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface EmployeeRepository extends ReactiveMongoRepository<Employee, UUID> {
+
+    @Query("{ 'employeeId': ?0 }")
+    Mono<Employee> findByEmployeeId(String employeeId);
+
+}
