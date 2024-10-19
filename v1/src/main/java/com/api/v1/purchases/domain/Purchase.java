d94f5a891ce246cd12f08eb3c176f3c50e83d794
lazarodamasceno <Mylittleponey1@>
2024-10-18 21:37:09 -0300

    Created the repository PurchaseRepository

--- v1/src/main/java/com/api/v1/purchases/domain/Purchase.java
+++ v1/src/main/java/com/api/v1/purchases/domain/Purchase.java
@@ -2,6 +2,9 @@ package com.api.v1.purchases.domain;
 
 import com.api.v1.customers.domain.Customer;
 import com.api.v1.employees.domain.Employee;
+import com.api.v1.purchases.utils.PurchaseOrderNumberGeneratorUtil;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import java.math.BigInteger;
@@ -9,13 +12,25 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.UUID;
 
-public record Purchase(
+@Getter
+@NoArgsConstructor
+public class  Purchase {
+
         @Id
-        UUID id,
-        BigInteger orderNumber,
-        Customer customer,
-        Employee salesperson,
-        LocalDateTime createdAt,
-        ZoneId createdAtZone
-) {
+        UUID id;
+        BigInteger orderNumber;
+        Customer customer;
+        Employee salesperson;
+        LocalDateTime createdAt;
+        ZoneId createdAtZone;
+
+        public Purchase(Customer customer, Employee salesperson) {
+                this.id = UUID.randomUUID();
+                this.orderNumber = PurchaseOrderNumberGeneratorUtil.generate();
+                this.customer = customer;
+                this.salesperson = salesperson;
+                this.createdAt = LocalDateTime.now();
+                this.createdAtZone = ZoneId.systemDefault();
+        }
+
 }
---
+++ v1/src/main/java/com/api/v1/purchases/domain/PurchaseRepository.java
@@ -0,0 +1,14 @@
+package com.api.v1.purchases.domain;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+import java.math.BigInteger;
+import java.util.UUID;
+
+public interface PurchaseRepository extends ReactiveMongoRepository<Purchase, UUID> {
+
+    @Query("{ 'orderNumber': ?0 }")
+    Mono<Purchase> findByOrderNumber(BigInteger orderNumber);
+}
