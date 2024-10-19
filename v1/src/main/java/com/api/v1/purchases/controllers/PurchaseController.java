def31d792a9bff136e696fe60d02589a6be58ea3
lazarodamasceno <Mylittleponey1@>
2024-10-18 21:33:45 -0300

    Created the controller PurchaseController

---
+++ v1/src/main/java/com/api/v1/purchases/controllers/PurchaseController.java
@@ -0,0 +1,14 @@
+package com.api.v1.purchases.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/purchases")
+@RequiredArgsConstructor
+public class PurchaseController {
+
+
+
+}
