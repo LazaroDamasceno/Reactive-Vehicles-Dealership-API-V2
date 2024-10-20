3385806d85339bee4d99697b84579ed96eedeed9
lazarodamasceno <Mylittleponey1@>
2024-10-20 19:43:32 -0300

    Created the test class CreditCardRegistrationTest

--- v1/src/main/java/com/api/v1/cards/utils/CardDueDate.java
+++ v1/src/main/java/com/api/v1/cards/utils/CardDueDate.java
@@ -1,10 +1,7 @@
 package com.api.v1.cards.utils;
 
-public record CardDueDate(int month, int year) {
-
-    @Override
-    public String toString() {
-        return "%s/%s".formatted(month, year);
-    }
-
+public record CardDueDate(
+        int month,
+        int year
+) {
 }
