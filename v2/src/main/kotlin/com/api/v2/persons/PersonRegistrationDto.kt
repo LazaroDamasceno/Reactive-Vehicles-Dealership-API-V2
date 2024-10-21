d688824d476e2392871316ed8577d2ed76e4c157
lazarodamasceno <Mylittleponey1@>
2024-10-21 03:05:04 -0300

    Created the dto PersonRegistrationDto

---
+++ v2/src/main/kotlin/com/api/v2/persons/PersonRegistrationDto.kt
@@ -0,0 +1,18 @@
+package com.api.v2.persons
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import java.time.LocalDate
+
+data class PersonRegistrationDto(
+    val firstName: @NotBlank String,
+    val middleName: String?,
+    val lastName: @NotBlank String,
+    val birthDate: LocalDate,
+    val ssn: String,
+    val email: @NotBlank @Email String,
+    val address: @NotBlank String,
+    val gender: @NotBlank @Size(min=1) String,
+    val phoneNumber: @NotBlank @Size(min=10, max=10) String
+)
