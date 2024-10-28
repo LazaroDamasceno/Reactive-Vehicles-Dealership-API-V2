f6dc78cf11677f1f06d33a048582c31c7cab9f67
lazarodamasceno <lazarolawrence2@gmail.com>
2024-10-28 00:25:12 -0300

    Created the entity Person and its repository

---
+++ kotlin/v3/src/main/kotlin/com/api/v3/persons/Person.kt
@@ -0,0 +1,51 @@
+package com.api.v3.persons
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+
+@Document
+class Person(
+    var firstName: String,
+    var middleName: String?,
+    var lastName: String,
+    var ssn: String,
+    var birthDate: LocalDate,
+    var email: String,
+    var address: String,
+    var gender: String,
+    var phoneNumber: String
+) {
+
+    @Id
+    val id: UUID = UUID.randomUUID()
+    val createdAt: LocalDateTime = LocalDateTime.now()
+    val createdAtZone: ZoneId = ZoneId.systemDefault()
+
+    companion object {
+        fun of(requestDto: PersonRegistrationRequestDto): Person {
+            return Person(
+                requestDto.firstName,
+                requestDto.middleName,
+                requestDto.lastName,
+                requestDto.ssn,
+                requestDto.birthDate,
+                requestDto.email,
+                requestDto.address,
+                requestDto.gender,
+                requestDto.phoneNumber
+            )
+        }
+    }
+
+    fun fullName(): String {
+        if (middleName.isNullOrEmpty()) {
+            return "$firstName $lastName"
+        }
+        return "$firstName $middleName $lastName"
+    }
+
+}
---
+++ kotlin/v3/src/main/kotlin/com/api/v3/persons/PersonRepository.kt
@@ -0,0 +1,15 @@
+package com.api.v3.persons
+
+import org.springframework.data.mongodb.repository.Query
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.util.UUID
+
+interface PersonRepository: CoroutineCrudRepository<Person, UUID> {
+
+    @Query("{ 'ssn': ?0 }")
+    fun findBySsn(ssn: String): Person?
+
+    @Query("{ 'email': ?0 }")
+    fun findByEmail(email: String): Person?
+
+}
