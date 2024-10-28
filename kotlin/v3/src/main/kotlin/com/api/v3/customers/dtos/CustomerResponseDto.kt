eb4386804b99d5f4efb5287c6cec40bb2a80f86f
lazarodamasceno <lazarolawrence2@gmail.com>
2024-10-28 20:22:57 -0300

    Deleted the folder 'customer'

--- kotlin/v3/src/main/kotlin/com/api/v3/customers/dtos/CustomerResponseDto.kt
+++
@@ -1,9 +0,0 @@
-package com.api.v3.customers.dtos
-
-import com.api.v3.persons.dtos.PersonResponseDto
-import java.time.ZonedDateTime
-
-data class CustomerResponseDto(
-    val personResponseDto: PersonResponseDto,
-    val createdAt: ZonedDateTime
-)
