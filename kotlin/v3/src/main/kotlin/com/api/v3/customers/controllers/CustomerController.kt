a5633b6a1b8d30c3d06efb548b37905710195bfc
lazarodamasceno <lazarolawrence2@gmail.com>
2024-10-28 02:52:25 -0300

    Created the service CustomerModificationService

--- kotlin/v3/src/main/kotlin/com/api/v3/customers/controllers/CustomerController.kt
+++ kotlin/v3/src/main/kotlin/com/api/v3/customers/controllers/CustomerController.kt
@@ -1,26 +1,35 @@
 package com.api.v3.customers.controllers
 
 import com.api.v3.customers.dtos.CustomerResponseDto
+import com.api.v3.customers.services.CustomerModificationService
 import com.api.v3.customers.services.CustomerRegistrationService
+import com.api.v3.persons.dtos.PersonModificationRequestDto
 import com.api.v3.persons.dtos.PersonRegistrationRequestDto
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v3/customers")
 class CustomerController(
-    private val registrationService: CustomerRegistrationService
+    private val registrationService: CustomerRegistrationService,
+    private val customerModificationService: CustomerModificationService
 ) {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
-    suspend fun register(@RequestBody requestDto: @Valid PersonRegistrationRequestDto): CustomerResponseDto {
+    suspend fun register(
+        @RequestBody requestDto: @Valid PersonRegistrationRequestDto
+    ): CustomerResponseDto {
         return registrationService.register(requestDto)
+    }
+
+    @PutMapping("{ssn}")
+    suspend fun modify(
+        @PathVariable ssn: String,
+        @RequestBody requestDto: @Valid PersonModificationRequestDto
+    ): CustomerResponseDto {
+        return customerModificationService.modify(ssn, requestDto)
     }
 
 }
