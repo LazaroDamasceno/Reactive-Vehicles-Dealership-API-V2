82c3f33a6742703c046138fe4818fde5a4f4c368
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2024-10-29 01:26:03 -0300

    Delete c#/V4 directory

--- c#/V4/Persons/Exceptions/DuplicatedEmailException.cs
+++
@@ -1,3 +0,0 @@
-namespace V4.Persons.Exceptions;
-
-public class DuplicatedEmailException(): Exception("The given email is already in use");
--- c#/V4/Persons/Exceptions/DuplicatedSsnException.cs
+++
@@ -1,3 +0,0 @@
-namespace V4.Persons.Exceptions;
-
-public class DuplicatedSsnException(): Exception("The given SSN is already in use");
